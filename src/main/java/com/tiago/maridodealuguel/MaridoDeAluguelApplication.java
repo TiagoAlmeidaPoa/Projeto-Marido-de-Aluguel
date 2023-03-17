8c9b7f1b098cd22f3a99222c92f203ac4f0a79f6
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-03-17 19:30:56 -0300

    criando projeto marido-de-aluguel

---
+++ src/main/java/com/tiago/maridodealuguel/MaridoDeAluguelApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.maridodealuguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaridoDeAluguelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaridoDeAluguelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiago/maridodealuguel/MaridoDeAluguelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.maridodealuguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaridoDeAluguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
