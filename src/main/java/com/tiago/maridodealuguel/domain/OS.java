c2b8cc9a5096ecf38b78357558c0f5135aeda3af
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-03-20 21:25:43 -0300

    Criando classe OS

---
+++ src/main/java/com/tiago/maridodealuguel/domain/OS.java
@@ -0,0 +1,130 @@
+package com.tiago.maridodealuguel.domain;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import com.tiago.maridodealuguel.domain.enums.Prioridade;
+import com.tiago.maridodealuguel.domain.enums.Status;
+
+public class OS {
+
+	private Integer id;
+	private LocalDateTime dataAbertura;
+	private LocalDateTime dataFechamento;
+	private Integer prioridade;
+	private Integer status;
+	private String observacoes;
+	private Operador operador;
+	private Cliente cliente;
+	private Double preco;
+
+	public OS() {
+		super();
+		this.setDataAbertura(LocalDateTime.now());
+		this.setPrioridade(Prioridade.BAIXA);
+		this.setStatus(Status.ABERTO);
+	}
+
+	public OS(Integer id, Prioridade prioridade, Status status, String observacoes, Operador operador, Cliente cliente,
+			Double preco) {
+		super();
+		this.id = id;
+		this.setDataAbertura(LocalDateTime.now());
+		this.prioridade = (prioridade == null) ? 0 : prioridade.getCod();
+		this.status = (status == null) ? 0 : status.getCod();
+		this.observacoes = observacoes;
+		this.operador = operador;
+		this.cliente = cliente;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public LocalDateTime getDataFechamento() {
+		return dataFechamento;
+	}
+
+	public void setDataFechamento(LocalDateTime dataFechamento) {
+		this.dataFechamento = dataFechamento;
+	}
+
+	public Prioridade getPrioridade() {
+		return Prioridade.toEnum(this.prioridade);
+	}
+
+	public void setPrioridade(Prioridade prioridade) {
+		this.prioridade = prioridade.getCod();
+	}
+
+	public Status getStatus() {
+		return Status.toEnum(this.status);
+	}
+
+	public void setStatus(Status status) {
+		this.status = status.getCod();
+	}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+	public Operador getOperador() {
+		return operador;
+	}
+
+	public void setOperador(Operador operador) {
+		this.operador = operador;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OS other = (OS) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
