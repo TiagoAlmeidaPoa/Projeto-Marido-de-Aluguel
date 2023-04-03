b59f81fc2b3b6196348ca6e9690c755041aa6d84
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-04-03 20:03:15 -0300

    Criando endpoint update PUT, para Operador

--- src/main/java/com/tiago/maridodealuguel/controller/OperadorController.java
+++ src/main/java/com/tiago/maridodealuguel/controller/OperadorController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,5 +45,11 @@ public class OperadorController {
 		Operador operador = service.create(operadorDTO);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(operador.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<OperadorDTO> update(@PathVariable Integer id,@Valid @RequestBody OperadorDTO objDTO) {
+		OperadorDTO newObj = new OperadorDTO(service.update(id, objDTO));
+		return ResponseEntity.ok().body(newObj);		
 	}
 }
--- src/main/java/com/tiago/maridodealuguel/service/OperadorService.java
+++ src/main/java/com/tiago/maridodealuguel/service/OperadorService.java
@@ -3,6 +3,8 @@ package com.tiago.maridodealuguel.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,13 +30,28 @@ public class OperadorService {
 	}
 
 	public Operador create(OperadorDTO operadorDTO) {
-		if(findByCPF(operadorDTO) != null) {
+		if (findByCPF(operadorDTO) != null) {
 			throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!");
 		}
-		return repository.save(new Operador(null, operadorDTO.getNome(), operadorDTO.getCpf(), operadorDTO.getTelefone()));
-		
+		return repository
+				.save(new Operador(null, operadorDTO.getNome(), operadorDTO.getCpf(), operadorDTO.getTelefone()));
+
 	}
 	
+	public Operador update(Integer id, @Valid OperadorDTO objDTO) {
+		Operador oldObj = findById(id);
+		
+		if(findByCPF(objDTO) != null && findByCPF(objDTO).getId() != id) {
+			throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!");
+		}
+		
+		oldObj.setCpf(objDTO.getCpf());
+		oldObj.setName(objDTO.getNome());
+		oldObj.setTelefone(objDTO.getTelefone());
+		
+		return repository.save(oldObj);
+	}
+
 	public Operador findByCPF(OperadorDTO objDTO) {
 		Operador obj = repository.findByCPF(objDTO.getCpf());
 		return (obj != null) ? obj : null;
