74671bef8891598fe2ec7898005905e9cd580ef2
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-04-07 21:02:34 -0300

    feat: criando endpoint create para Cliente

--- src/main/java/com/tiago/maridodealuguel/controller/ClienteController.java
+++ src/main/java/com/tiago/maridodealuguel/controller/ClienteController.java
@@ -1,14 +1,21 @@
 package com.tiago.maridodealuguel.controller;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.tiago.maridodealuguel.domain.Cliente;
 import com.tiago.maridodealuguel.dtos.ClienteDTO;
 import com.tiago.maridodealuguel.service.ClienteService;
 
@@ -29,6 +36,13 @@ public class ClienteController {
 	public ResponseEntity<List<ClienteDTO>> findAll(){
 		List<ClienteDTO> dtos = service.findAll();
 		return ResponseEntity.ok().body(dtos);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClienteDTO> create(@Valid @RequestBody ClienteDTO clienteDTO){
+		Cliente	cliente = service.create(clienteDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(cliente.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/tiago/maridodealuguel/service/ClienteService.java
+++ src/main/java/com/tiago/maridodealuguel/service/ClienteService.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tiago.maridodealuguel.domain.Cliente;
+import com.tiago.maridodealuguel.domain.Pessoa;
 import com.tiago.maridodealuguel.dtos.ClienteDTO;
 import com.tiago.maridodealuguel.repositories.ClienteRepository;
+import com.tiago.maridodealuguel.repositories.PessoaRepository;
 import com.tiago.maridodealuguel.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -17,6 +19,9 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repository;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
 	
 	public Cliente findById(Integer id) {
 		Optional<Cliente> obj = repository.findById(id);
@@ -29,6 +34,17 @@ public class ClienteService {
 		List<ClienteDTO> dtos = repository.findAll().stream().map(x -> new ClienteDTO(x)).collect(Collectors.toList());
 		return dtos;
 	}
+
+	public Cliente create(ClienteDTO clienteDTO) {
+		if(findByCPF(clienteDTO) != null) {
+			throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!");
+		}
+		return repository.save(new Cliente(null, clienteDTO.getNome(), clienteDTO.getCpf(), clienteDTO.getTelefone()));
+	}
 	
+	public Pessoa findByCPF(ClienteDTO objDTO) {
+		Pessoa obj = pessoaRepository.findByCPF(objDTO.getCpf());
+		return (obj != null) ? obj : null;
+	}
 
 }
